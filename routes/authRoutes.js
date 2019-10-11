0cda866c97d6211bb0a0be0bd3ba5289ce3832b1
Sabit <sabit990928@gmail.com>
2019-10-11 17:03:15 +0600

    feat(authRoutes): add logout and current_user api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,14 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google')); // there is code in request url like ?code=sdsfj39i4
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user)
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    // res.send(req.session)
+    res.send(req.user);
+  });
 };
