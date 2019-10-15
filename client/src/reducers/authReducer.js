470cf735c1ad11b650defeb4c78c11b7b3b2ec1a
Sabit <sabit990928@gmail.com>
2019-10-15 18:38:10 +0600

    feat: introduce reducers

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,15 @@
+import { FETCH_USER } from '../actions/types';
+
+// const InitialState = {
+// 	loading:
+// }
+
+export default function(state = null, action) {
+  console.log('action: ', action);
+  switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
+    default:
+      return state;
+  }
+}
