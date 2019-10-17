32c89c095c41dd0971a9e4e65e9492749202cf5d
Sabit <sabit990928@gmail.com>
2019-10-17 21:10:25 +0600

    feat: add sendgrid api keys

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
+import axios from 'axios';
 import App from './components/App';
 import reducers from './reducers';
+// Development only axios helpers!
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
