6c34148709bcd76d12b30bf961a4e82105c0ccf6
Sabit <sabit990928@gmail.com>
2019-10-17 21:08:55 +0600

    feat: intoduce Dashboard component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import Dashboard from './Dashboard';
 import Header from './Header';
 import Landing from './Landing';
 import * as actions from '../actions';
 
-const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 
 class App extends Component {
