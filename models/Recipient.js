8dc4a007bc61550d2e70fac222d8ad3921e5ee2f
Sabit <sabit990928@gmail.com>
2019-10-17 21:11:02 +0600

    feat: introduce Recipient model

---
+++ models/Recipient.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const recipientSchema = new Schema({
+  email: { type: String, lowercase: true, trim: true },
+  responded: { type: Boolean, default: false },
+});
+
+module.exports = recipientSchema;
+// mongoose.model('recipients', recipientSchema);
