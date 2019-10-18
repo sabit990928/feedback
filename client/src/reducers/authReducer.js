771cc69a0debb737acc55a235629ef252744fc28
Sabit <sabit990928@gmail.com>
2019-10-18 23:48:31 +0600

    feat: add lodash library

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -5,7 +5,7 @@ import { FETCH_USER } from '../actions/types';
 // }
 
 export default function(state = null, action) {
-  console.log('action: ', action);
+  // console.log('action: ', action);
   switch (action.type) {
     case FETCH_USER:
       return action.payload || false;
