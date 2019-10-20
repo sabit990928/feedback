c417b8e3f623cead88a55029968081ce526e0bb6
Sabit <sabit990928@gmail.com>
2019-10-20 22:07:21 +0600

    feat(SurveyNew): add redux form

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
@@ -28,4 +30,6 @@ class SurveyNew extends Component {
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: 'surveyForm',
+})(SurveyNew);
