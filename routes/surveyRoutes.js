b661e9240ccc5ad3029871e10d07835d4a2d2303
Sabit <sabit990928@gmail.com>
2019-10-21 12:57:58 +0600

    feat(surveyRoutes): handle webhook events and parsing the path, take unique elements

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireUser = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -12,6 +15,29 @@ module.exports = app => {
 
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    // all events in req.body
+    const events = _.map(req.body, ({ email, url }) => {
+      const { pathname } = new URL(url);
+      const p = new Path('/api/surveys/:surveyId/:choice');
+      const match = p.test(pathname);
+      // console.log('test:', p.test(pathname));
+      if (match) {
+        return {
+          email,
+          surveyId: match.surveyId,
+          choice: match.choice,
+        };
+      }
+    });
+    const compactEvents = _.compact(events); // remove undefined values
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+    console.log('unique: ', uniqueEvents);
+    console.log('events: ', events);
+    res.send({});
   });
 
   app.post('/api/surveys', requireUser, requireCredits, async (req, res) => {
